0b12728de244f674846f426af2b9870070223407
vitoryeso <vitoryeso@outlook.com>
2022-07-21 22:03:39 -0300

    password from dotenv

--- db_populate.py
+++ db_populate.py
@@ -1,20 +1,27 @@
 # Module Imports
 import mariadb
 import sys
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Connect to MariaDB Platform
 try:
+    print(os.getenv("DB_PASSWORD"))
     conn = mariadb.connect(
         user="vyeso",
-        password="wowo",
+        password=os.getenv("DB_PASSWORD"),
         host="localhost",
         port=3306,
-        database="employees"
+        database="spotify"
 
     )
+    print("connected!")
 except mariadb.Error as e:
     print(f"Error connecting to MariaDB Platform: {e}")
     sys.exit(1)
 
 # Get Cursor
 cur = conn.cursor()
+print(cur)
